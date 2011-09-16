650bb3e5d1de275c3e9086881114e39d6027d3b3
groovecoder <luke.crouch@gmail.com>
2011-09-16 13:09:58 -0700

    basic change to 'deviceorientation'; buggy

--- pixels/script.js
+++ pixels/script.js
@@ -1,5 +1,5 @@
-const X_Amplitude = 15;
-const Y_Amplitude = 15;
+const X_Amplitude = 30;
+const Y_Amplitude = 30;
 
 var row1, row2, row3, d1, d2, d3;
 
@@ -62,21 +62,20 @@ function start3D() {
     initialized = true;
     document.body.classList.remove("no3D");
 
-    window.addEventListener("MozOrientation", function(e) {
-        if (!landscape) {
-            xshift = e.x * -2.5;
-            yshift = (e.y - 0.5) * -2.5;
-        } else {
-            xshift = e.y * -2.5;
-            yshift = (e.x + 0.7) * 2.5;
-        }
+    window.addEventListener("deviceorientation", function(e) {
+        gd = document.getElementById('gamma_display');
+        gd.innerHTML= e.gamma;
+        bd = document.getElementById('beta_display');
+        bd.innerHTML= e.gamma;
+        xshift = e.gamma/90 * 2.5;
+        yshift = e.beta/90 * 2.5;
     }, true);
 
 
     for (var i = 0; i < layers.length; i++) {
         var l = layers[i];
         var z = l.getAttribute("z");
-        l.style.MozTransform = "scale(" + (scale + (1 - scale) * (z)) + ")";
+        l.style.transform = "scale(" + (scale + (1 - scale) * (z)) + ")";
         l.style.zIndex = 1000 * z;
     }
     draw();
@@ -88,10 +87,6 @@ var oldY = 1000;
 
 function draw() {
     landscape = (window.innerHeight < window.innerWidth);
-    if (xshift > 1) xshift = 1;
-    if (xshift < -1) xshift = -1;
-    if (yshift > 1) yshift = 1;
-    if (yshift < -1) yshift = -1;
 
     oldX = xshift;
     oldY = yshift;
@@ -99,12 +94,12 @@ function draw() {
     marginTop = (row1.orgHeight + Y_Amplitude * yshift);
     row1.style.height =  marginTop + "%";
 
-    row3.style.height = (row3.orgHeight - Y_Amplitude * yshift) + "%";
+    row3.style.height = (row3.orgHeight + Y_Amplitude * yshift) + "%";
 
     marginLeft = ds[0].orgWidth + X_Amplitude * xshift;
     for (var i = 0, ii = d1.length; i < ii; i++) { d1[i].style.width = marginLeft + "%"; }
 
-    var width = (d3[0].orgWidth - X_Amplitude * xshift) + "%";
+    var width = (d3[0].orgWidth + X_Amplitude * xshift) + "%";
     for (var i = 0, ii = d3.length; i < ii; i++) {d3[i].style.width = width;}
 
     projectLayers();
